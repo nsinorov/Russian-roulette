f87b41aaaf7926621e7d30891015c60e6871b377
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-08 00:31:28 +0200

    Add files via upload

---
+++ Russian roulette/Russian roulette/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RussianRoulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int pulltriger = rnd.Next(1, 7); // Choose a random number from 1 to 6
+            int chamber = rnd.Next(1, 7); 
+            
+            if (chamber == pulltriger)
+            {
+                Console.WriteLine("Bang! You are dead.");
+            }
+            else
+            {
+                Console.WriteLine("Click. You live, for now");
+            }
+        }
+    }
+}
